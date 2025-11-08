d9c77f6a57012f95390f80f9eba9185c487b9788
sakshii35 <sakshiimonga888@gmail.com>
2025-11-08 14:42:44 +0530

    Create App.js

---
+++ ecommerce-pipeline/dashboard/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import OrderTimeline from './OrderTimeline';
+
+function App() {
+  return (
+    <div className="App">
+      <OrderTimeline />
+    </div>
+  );
+}
+
+export default App;
